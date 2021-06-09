c402b041cb04239dc1d5ecc9ded4c79250c53732
Shanen Cross <shanencross@yahoo.com>
2021-06-08 20:28:08 -0700

    Make properties public
    Co-authored-by: Salim Mayan <mailsalim@gmail.com>

--- Library/Models/AuthorBook.cs
+++ Library/Models/AuthorBook.cs
@@ -2,9 +2,9 @@ namespace Library.Models
 {
   public class AuthorBook
   {
-    int AuthorBookId { get; set; }
-    int BookId { get; set; }
-    int AuthorId { get; set; }
+    public int AuthorBookId { get; set; }
+    public int BookId { get; set; }
+    public int AuthorId { get; set; }
     public virtual Author Author { get; set; }
     public virtual Book Book { get; set; }
   }
