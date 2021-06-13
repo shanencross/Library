ad05f41536d19fb3a647fe55538401152e7d6af8
Shanen Cross <shanencross@yahoo.com>
2021-06-13 16:12:32 -0700

    Add boolean to check checkout status
    Co-authored-by: Salim Mayan <mailsalim@gmail.com>

--- Library/Models/Copy.cs
+++ Library/Models/Copy.cs
@@ -6,11 +6,13 @@ namespace Library.Models
   {
     public int CopyId { get; set; }
     public int BookId { get; set; }
+    public bool IsCheckedOut { get; set; }
     public virtual ICollection<Checkout> Checkouts { get; set; }
     
     public Copy()
     {
       this.Checkouts = new HashSet<Checkout>();
+      this.IsCheckedOut = false;
     }
   }
 }
