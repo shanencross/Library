719ed0afa092e8db75805460d0b431b5b1770175
Shanen Cross <shanencross@yahoo.com>
2021-06-13 16:14:07 -0700

    Add new migration for checkout check
    Co-authored-by: Salim Mayan <mailsalim@gmail.com>

---
+++ Library/Migrations/20210613223449_AddIsCheckedOut.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Library.Migrations
+{
+    public partial class AddIsCheckedOut : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCheckedOut",
+                table: "Copies",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCheckedOut",
+                table: "Copies");
+        }
+    }
+}
