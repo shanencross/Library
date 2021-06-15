c20cd35c0aff672ed8ce3e81a28d2f572b97e494
Shanen Cross <shanencross@yahoo.com>
2021-06-14 19:55:50 -0700

    Add new migration for adding book reference to copy
    Co-authored-by: Salim Mayan <mailsalim@gmail.com>

---
+++ Library/Migrations/20210615025525_AddBookPropertyToCopy.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Library.Migrations
+{
+    public partial class AddBookPropertyToCopy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Checkouts",
+                type: "varchar(255) CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Checkouts_UserId",
+                table: "Checkouts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Checkouts_AspNetUsers_UserId",
+                table: "Checkouts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Checkouts_AspNetUsers_UserId",
+                table: "Checkouts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Checkouts_UserId",
+                table: "Checkouts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Checkouts");
+        }
+    }
+}
